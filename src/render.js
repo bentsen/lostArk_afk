ad7126fdcb62b35fd08bb7edd37a9ae349d87636
Mikkel Bentsen <cph-mb781@cphbusiness.dk>
2022-03-01 21:23:19 +0100

    fix

--- src/render.js
+++ src/render.js
@@ -1,4 +1,4 @@
-//import { robot } from 'robotjs';  //dosent work
+import { robot } from 'robotjs';
 import { remote } from 'electron';
 
 const buttonStart = document.getElementById('start');
@@ -34,7 +34,7 @@ buttonClose.addEventListener('click', event => {
 function start()
 {   
      timeOut = setInterval(() => {
-        console.log("habibi");
+        move();
     }, (Math.random() * (900 - 600)) + 600);
 }
 
@@ -44,8 +44,6 @@ function move()
     console.log("move er started");
     const height = window.screen.availHeight + 100/2;
     const width = window.screen.availWidth - 100/2; 
-
-    //TODO make a 'rectangle' for the poistions that the cusor will be able to click
 
     const x = (Math.random() * (100 - 0) + 0) + width;
     const y = (Math.random() * (100 - 0) + 0) + height;
